0b45178c96cab443a7a20500f2e1d5efa9bb2180
Caner Burç BAŞKAYA <canertutoring@gmail.com>
2020-07-07 14:30:05 +0300

    layout changes

--- app/src/main/java/com/lydia/imageembeddedcryptographer/MainActivity.kt
+++ app/src/main/java/com/lydia/imageembeddedcryptographer/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.lydia.imageembeddedcryptographer
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import android.view.inputmethod.InputMethodManager
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        hideKeyboard()
+
+    }
+
+
+    fun hideKeyboard() {
+        val view = this.currentFocus
+        if (view != null) {
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(view.windowToken, 0)
+        }
+        // else {
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN)
+        // }
     }
 }
