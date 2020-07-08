2a0adfa9886fe467f4ee8c0d040fd0f392211a72
Caner Burç BAŞKAYA <canertutoring@gmail.com>
2020-07-08 18:23:14 +0300

    Animations Added

---
+++ app/src/main/java/com/lydia/imageembeddedcryptographer/SoundUtlis.kt
@@ -0,0 +1,25 @@
+package com.lydia.imageembeddedcryptographer
+
+import android.content.Context
+import android.media.MediaPlayer
+
+class SoundUtlis {
+
+    companion object{
+
+        public enum class SOUNDS(val sourceID: Int){
+            CLICK(R.raw.menu_selection),
+            ENCODE(R.raw.swoosh_brighter),
+            DECODE(R.raw.menu_selection)
+
+        }
+
+        fun playSound(soundSourceID: Int, context: Context){
+            val mediaPlayer = MediaPlayer.create(context, soundSourceID)
+            mediaPlayer.setVolume(1.0f, 1.0f)
+            mediaPlayer.start()
+        }
+
+    }
+
+}
